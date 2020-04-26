28b1f59dc52625cf5d428a72f75375d31d52c82f
Hemil Parmar <hemilcp@gmail.com>
2020-04-26 00:13:17 -0700

    adding intput references and submit event handlers

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
 class TodoList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            tasks : []
+        }
+        this.addTask = this.addTask.bind(this);
+    }
+
+    addTask(e){
+        if(this._taskInputElement.value !== ""){
+            var newTask = {
+                text: this._taskInputElement.value.trim(),
+                key: Date.now()
+            };
+            this.setState((prevState)=>{
+                return{
+                    tasks: prevState.tasks.concat(newTask)
+                }
+            });
+            this._taskInputElement.value = "";
+        }
+        console.log("tasks: " + this.state.tasks);
+        e.preventDefault();
+    }
+
     render(){
         return(
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="enter task here">
+                    <form onSubmit={this.addTask}>
+                        <input ref={(a) => this._taskInputElement = a} placeholder="enter task here">
                         </input>
                         <button type="submit">Add</button>
                     </form>
